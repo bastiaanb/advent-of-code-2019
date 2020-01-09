48b42606baabcbed64b756476e80198849d72e92
Bastiaan Bakker <bbakker@xebia.com>
2020-01-09 17:54:15 +0100

    compacted day 10

--- day-10/star-19-and-20.py
+++ day-10/star-19-and-20.py
@@ -5,18 +5,19 @@ import math
 import numpy as np
 from sortedcontainers import SortedSet, SortedDict
 
-asteroids={}
-
 sign = lambda a: (a>0) - (a<0)
 
-y = 0
-for line in fileinput.input():
-    x = 0
-    for c in line.strip():
-        if c == '#':
-            asteroids[(x, y)]=0
-        x+=1
-    y+=1
+def read_input():
+    asteroids={}
+    y = 0
+    for line in fileinput.input():
+        x = 0
+        for c in line.strip():
+            if c == '#':
+                asteroids[(x, y)]=0
+            x+=1
+        y+=1
+    return asteroids
 
 def in_between(a, b):
     if a == b:
@@ -43,37 +44,37 @@ def is_visible(a, b):
 def count_visible(a):
     return sum(1 for b in asteroids.keys() if is_visible(a, b)) - 1
 
-max_pos, max_visible = None, 0
-for a in asteroids:
-    v = count_visible(a)
-    asteroids[a]=v
-    if v > max_visible:
-        max_visible = v
-        max_pos = a
+def best_asteroid():
+    max_pos, max_visible = None, 0
+    for a in asteroids:
+        v = count_visible(a)
+        asteroids[a]=v
+        if v > max_visible:
+            max_visible = v
+            max_pos = a
+    return max_pos, max_visible
 
+asteroids = read_input()
+max_pos, max_visible = best_asteroid()
 print(f"star 19: {max_visible} at {max_pos}")
 
 def to_polar(a):
     x,y = a
     return np.sqrt(x**2+y**2), (np.pi + np.arctan2(-x, y)) % (2 * np.pi)
 
-del asteroids[max_pos]
-polar_asteroids=SortedDict()
-for a in asteroids:
-    r, t = to_polar((a[0] - max_pos[0], a[1] - max_pos[1]))
-    if t in polar_asteroids:
-        polar_asteroids[t][r] = a
-    else:
-        polar_asteroids[t] = SortedDict({r: a})
+def order_asteroids():
+    polar_asteroids=SortedDict()
+    for a in asteroids:
+        r, t = to_polar((a[0] - max_pos[0], a[1] - max_pos[1]))
+        polar_asteroids.setdefault(t, SortedDict())[r] = a
 
-i = 1
-while i < 200:
-    for t, rs in polar_asteroids.items():
-        if len(rs) > 0:
-            r = rs.keys()[0]
-            a = rs.pop(r)
-#            print(f"{i} ({t},{r}): {a}")
-            if i == 200:
-                print(f"star 20: {a[0] * 100 + a[1]}")
-                break
-            i+=1
+    while polar_asteroids:
+        for t, rs in list(polar_asteroids.items()):
+            a = rs.pop(rs.keys()[0])
+            if not rs:
+                del polar_asteroids[t]
+            yield a
+
+del asteroids[max_pos]
+a200 = list(order_asteroids())[199]
+print(f"star 20: {a200[0] * 100 + a200[1]}")
